916ca5bcd521d47c47475ad0571a3fd88af946da
Phill <phillicia.mitchell21@gmail.com>
2020-11-08 20:10:53 -0500

    imported Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <div class="container">
-        <b-navbar>
-          <div class="container"></div>
-          <template slot="brand" class="primary">
-            <b-navbar-item>
-              <span class="logo">Phill<span>Mitchell</span></span>
-            </b-navbar-item>
-          </template>
-
-          <!-- These links will colapes -->
-          <template slot="end">
-            <b-navbar-item>
-              home
-            </b-navbar-item>
-            <b-navbar-item>
-              portfolio
-            </b-navbar-item>
-            <b-navbar-item>
-              about
-            </b-navbar-item>
-            <b-navbar-item>
-              contact
-            </b-navbar-item>
-          </template>
-        </b-navbar>
-      </div>
-    </div>
+    <Navbar />
     <router-view />
   </div>
 </template>
 
-<script></script>
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  name: "app",
+  components: {
+    Navbar
+  }
+};
+</script>
