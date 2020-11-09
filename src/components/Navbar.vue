6d84564dfaf6465bf5a2dd39b33cda0f557c331a
Phill <phillicia.mitchell21@gmail.com>
2020-11-08 20:10:23 -0500

    moves navbar code to components folder and independent file

---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="nav">
+    <div class="container">
+      <b-navbar>
+        <div class="container"></div>
+        <template slot="brand" class="primary">
+          <b-navbar-item>
+            <span class="logo">Phill<span>Mitchell</span></span>
+          </b-navbar-item>
+        </template>
+
+        <!-- These links will colapes -->
+        <template slot="end">
+          <b-navbar-item>
+            home
+          </b-navbar-item>
+          <b-navbar-item>
+            portfolio
+          </b-navbar-item>
+          <b-navbar-item>
+            about
+          </b-navbar-item>
+          <b-navbar-item>
+            contact
+          </b-navbar-item>
+        </template>
+      </b-navbar>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
