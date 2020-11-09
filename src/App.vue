0500ee9643acd48171a6de3649c2f8abd33e9e27
Phill <phillicia.mitchell21@gmail.com>
2020-11-08 19:41:56 -0500

    put navbar into a container

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div id="app">
     <div id="nav">
-      <b-navbar>
-        <template slot="brand" class="primary">
-          <b-navbar-item>
-            <span class="logo">Phill<span>Mitchell</span></span>
-          </b-navbar-item>
-        </template>
+      <div class="container">
+        <b-navbar>
+          <div class="container"></div>
+          <template slot="brand" class="primary">
+            <b-navbar-item>
+              <span class="logo">Phill<span>Mitchell</span></span>
+            </b-navbar-item>
+          </template>
 
-        <!-- These links will colapes -->
-        <template slot="end">
-          <b-navbar-item>
-            home
-          </b-navbar-item>
-          <b-navbar-item>
-            portfolio
-          </b-navbar-item>
-          <b-navbar-item>
-            about
-          </b-navbar-item>
-          <b-navbar-item>
-            contact
-          </b-navbar-item>
-        </template>
-      </b-navbar>
+          <!-- These links will colapes -->
+          <template slot="end">
+            <b-navbar-item>
+              home
+            </b-navbar-item>
+            <b-navbar-item>
+              portfolio
+            </b-navbar-item>
+            <b-navbar-item>
+              about
+            </b-navbar-item>
+            <b-navbar-item>
+              contact
+            </b-navbar-item>
+          </template>
+        </b-navbar>
+      </div>
     </div>
     <router-view />
   </div>
