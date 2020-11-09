595a01cf0f3baa0bde461c3b29740b56f7bdc541
Phill <phillicia.mitchell21@gmail.com>
2020-11-08 23:21:12 -0500

    added services section elements

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Hero />
+    <Services />
     <h1>First Vue App</h1>
   </div>
 </template>
 
 <script>
 import Hero from "./Hero";
+import Services from "./Services";
 
 export default {
   name: "Home",
   components: {
-    Hero
+    Hero,
+    Services
   }
 };
 </script>
