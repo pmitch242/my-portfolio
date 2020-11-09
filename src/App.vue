e584e90a2b8dd7f2ae5cfb0d20fde56feb45e461
Phill <phillicia.mitchell21@gmail.com>
2020-11-09 02:46:15 -0500

    import background image

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <Navbar />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 export default {
   name: "app",
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="footer">
+    <img src="../images/footer-bg.png" alt="footer-bg" />
+    <div class="container">
+      <div class="footer-span-container">
+        <span id="logo">Phill<span class="">Mitchell</span></span>
+        <span class="subtitle">
+          Website designed and coded by me... I am me.
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style scoped>
+.footer-span-container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
