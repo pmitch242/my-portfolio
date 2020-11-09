3b96cce07dbe30e165e0705cc382ab35b780cf8c
Phill <phillicia.mitchell21@gmail.com>
2020-11-08 21:26:46 -0500

    added hero elements

---
+++ src/views/Home/Hero.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="hero blue is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-one-half">
+            <span class="subtitle">Testing</span>
+            <h1>I design and build websites</h1>
+            <p>
+              With full stack experience, I devote my time to designing
+              beautiful digital products and making them come to life.
+            </p>
+            <div class="buttons">
+              <button class="button is-primary is-outlined">Outlined</button>
+              <button class="button is-primary">Primary</button>
+            </div>
+          </div>
+          <div class="column">Place Illustration here</div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Hero"
+};
+</script>
+
+<style>
+.hero {
+  background-color: #264653;
+  color: #ffffff;
+}
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="home">
+    <Hero />
     <h1>First Vue App</h1>
   </div>
 </template>
 
 <script>
+import Hero from "./Hero";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    Hero
+  }
 };
 </script>
